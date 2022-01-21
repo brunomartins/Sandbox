b855ae0d11ecd6e482fc74ad2a3e629c56e98c8f
Mirco Bianchini <sonomirco@gmail.com>
2022-01-21 14:04:42 +1100

    Added LineConverter for Rhino. Added comments for the LineConverter for Dy.

--- SandboxDynUtilities/Converters/LineConverter.cs
+++ SandboxDynUtilities/Converters/LineConverter.cs
@@ -7,11 +7,21 @@ namespace SandboxDynUtilities.Converters
     [IsVisibleInDynamoLibrary(false)]
     public static class LineConverter
     {
+        /// <summary>
+        /// Converts a Dynamo <see cref="Ad.Line"/> into a GShark <see cref="Line"/>.
+        /// </summary>
+        /// <param name="line">Dynamo line.</param>
+        /// <returns>GShark line.</returns>
         public static Line DyToGs(this Ad.Line line)
         {
             return new Line(line.StartPoint.DyToGs(), line.EndPoint.DyToGs());
         }
 
+        /// <summary>
+        /// Converts a GShark <see cref="Line"/> into a Dynamo <see cref="Ad.Line"/>.
+        /// </summary>
+        /// <param name="line">GShark point.</param>
+        /// <returns>Dynamo line.</returns>
         public static Ad.Line GsToDy(this Line line)
         {
             return Ad.Line.ByStartPointEndPoint(line.StartPoint.GsToDy(), line.EndPoint.GsToDy());
