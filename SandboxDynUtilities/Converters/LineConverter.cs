44e170d19188c6d400458ce8c861c7c8304660d5
Mirco Bianchini <sonomirco@gmail.com>
2022-01-21 13:58:35 +1100

    Added LineCorverter for Dynamo.

---
+++ SandboxDynUtilities/Converters/LineConverter.cs
@@ -0,0 +1,20 @@
+﻿using Autodesk.DesignScript.Runtime;
+using GShark.Geometry;
+using Ad = Autodesk.DesignScript.Geometry;
+
+namespace SandboxDynUtilities.Converters
+{
+    [IsVisibleInDynamoLibrary(false)]
+    public static class LineConverter
+    {
+        public static Line DyToGs(this Ad.Line line)
+        {
+            return new Line(line.StartPoint.DyToGs(), line.EndPoint.DyToGs());
+        }
+
+        public static Ad.Line GsToDy(this Line line)
+        {
+            return Ad.Line.ByStartPointEndPoint(line.StartPoint.GsToDy(), line.EndPoint.GsToDy());
+        }
+    }
+}
