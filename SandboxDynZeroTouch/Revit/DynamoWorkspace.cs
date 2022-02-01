c292373e06c188cef872dbda73fe72419398ca98
Ash Mason <57358910+ashmason8@users.noreply.github.com>
2022-02-01 20:57:15 +0000

    added manual flow control

---
+++ SandboxDynZeroTouch/Revit/DynamoWorkspace.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Dynamo;
+using Dynamo.Graph.Workspaces;
+
+namespace Sandbox.Revit
+{
+    public static class DynamoWorkspace
+    {
+
+        private static HomeWorkspaceModel _model;
+        private static bool activeEvaluate = false;
+
+        internal static void Register(HomeWorkspaceModel dynamoModel)
+        {
+            // Subscribe our event handler methods to Dynamo
+            dynamoModel.EvaluationCompleted += OnEvaulationCompleted;
+            _model = dynamoModel;
+        }
+        internal static void Unregister()
+        {
+            // Unsubscribe our event handler methods
+            _model.EvaluationCompleted -= OnEvaulationCompleted;
+        }
+
+        /// <summary>
+        /// When the graph is evaluated .. ?
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="evaluationCompletedEventArgs"></param>
+        private static void OnEvaulationCompleted(object sender, Dynamo.Models.EvaluationCompletedEventArgs evaluationCompletedEventArgs)
+        {
+
+            var model = Dynamo.Applications.DynamoRevit.RevitDynamoModel.CurrentWorkspace as HomeWorkspaceModel;
+            int NodeCount = 0;
+            foreach (Dynamo.Graph.Nodes.NodeModel node in model.Nodes)
+            {
+
+                if (node.CreationName.ToString() == "Sandbox.Revit.DynamoWorkspace.FlowAllowedIfManual@var[]")
+                {
+                    NodeCount++;
+                }
+                if (node.CreationName.ToString() == "Sandbox.Revit.DynamoWorkspace.FlowAllowedIfManual@var[]")
+                {
+                    node.MarkNodeAsModified(true);
+                }
+
+            }
+            
+            if (NodeCount == 0)
+            {
+                Unregister();
+                activeEvaluate = false;
+            }
+            
+        }
+
+        /// <summary>
+        /// Graph must be in manual mode to pass this node, otherwise an empty list is returned. Must be used with "ForceIndiviadualNodeRun" renamed with a prfix of "*Force".
+        /// </summary>
+        /// <param name="List">Any List.</param>
+        /// <param name="Out">Returns empty list if graph in manual.</param>
+        /// <returns>A sheet read to be written.</returns>
+        /// <search>flow, control</search>
+        public static object FlowAllowedIfManual(List<object> list)
+        {
+            var model = Dynamo.Applications.DynamoRevit.RevitDynamoModel.CurrentWorkspace as HomeWorkspaceModel;
+            if (activeEvaluate == false)
+            {
+                Register(model);
+                activeEvaluate = true;
+            }
+            List<string> l = new List<string>();
+            if (model.RunSettings.RunType.ToString() == "Manual")
+            {
+
+            }
+            else
+            {
+                list.Clear();
+            }
+            return list;
+        }
+
+    }
+}
