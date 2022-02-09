2898881f54e04cf9dd41c99d892a4adef0df81c8
William Hamilton <william.hamilton@mottmac.com>
2022-02-09 16:05:28 +1100

    Created the ValueType component for the dictionary project

--- SandboxGh/Resources.Designer.cs
+++ SandboxGh/Resources.Designer.cs
@@ -219,5 +219,15 @@ namespace SandboxGh {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap ValueTypeDictIcon {
+            get {
+                object obj = ResourceManager.GetObject("ValueTypeDictIcon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
---
+++ SandboxGh/Utility/ValueTypeDictionary.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types;
+using SandboxGh.Attributes;
+
+namespace SandboxGh.Utility
+{
+    public class ValueTypeDictionary : SandboxComponent
+    {
+        public ValueTypeDictionary()
+          : base("This component returns the type of the values in a dictionary, eg. 'text' or 'curve'", "Utilities")
+        {
+        }
+
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddParameter(new DictParam(), "Dict", "D", "The dictionary to extract the value types from.", GH_ParamAccess.item);
+        }
+
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddTextParameter("Types", "T", "The types of the values.", GH_ParamAccess.list);
+        }
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            var ghDict = new GH_Dict();
+            List<string> types = new List<string>();
+
+
+            if (!DA.GetData(0, ref ghDict)) return;
+
+            foreach (var item in ghDict.Value.Values)
+            {
+                types.Add(item.TypeName);   
+            }
+
+            DA.SetDataList(0, types);
+
+        }
+
+        protected override System.Drawing.Bitmap Icon => Resources.ValueTypeDictIcon;
+
+        public override Guid ComponentGuid => new Guid("2DD78996-31C9-44A8-A884-BB4AAC9741A2");
+    }
+}
