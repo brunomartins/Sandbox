6e430dccb4e8bdb7fdf30384cb77f5f265e2cf24
Mirco Bianchini <sonomirco@gmail.com>
2022-02-15 22:15:41 +1100

    Added methods to get the Gh and Dy directories.

--- SandboxCore/Utilities/Package.cs
+++ SandboxCore/Utilities/Package.cs
@@ -10,17 +10,27 @@ namespace SandboxCore.Utilities
 {
     public static class Package
     {
-        public static string GetSandboxVersion()
+        public static string DynamoDir()
+        {
+            var dynamoDir = @"\Dynamo\Dynamo Core";
+            var path = string.Concat(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), dynamoDir);
+            var file = Directory.GetFiles(path, "SandboxCore.dll", SearchOption.AllDirectories);
+       
+            return file.Length == 0 ? string.Empty : file[0];
+        }
+
+        public static string GrasshopperDir()
         {
             var fileDir = @"\Grasshopper\Libraries\SandboxGh\SandboxCore.dll";
-            var path = string.Concat(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), fileDir);
-            var version = string.Empty;
+            return string.Concat(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), fileDir);
+        }
 
-            if (!File.Exists(path)) return version;
+        public static string GetSandboxVersion(string path)
+        {
+            if (!File.Exists(path)) return string.Empty;
             FileVersionInfo versionInfo = FileVersionInfo.GetVersionInfo(path);
-            version = versionInfo.FileVersion;
 
-            return version;
+            return versionInfo.FileVersion;
         }
     }
 }
