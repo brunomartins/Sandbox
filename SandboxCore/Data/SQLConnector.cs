db390bbcde83edacae6c93321e36af364be992be
William Hamilton <william.hamilton@mottmac.com>
2022-03-01 18:08:40 +1100

    Started setting up and testing a connection to postgres server + grasshopper node

---
+++ SandboxCore/Data/SQLConnector.cs
@@ -0,0 +1,16 @@
+﻿using Npgsql;
+
+namespace SandboxCore.Data
+{
+    public static class SQLConnector
+    {
+        public static void SQLConnect(string server, string port, string uid, string pw, string db)
+        {
+            var cs = $"Host={server};Port={port};Username={uid};Password={pw};Database={db}";
+            
+            var con = new NpgsqlConnection(cs);
+            con.Open();
+
+        }
+    }
+}
---
+++ SandboxGh/Data/PostgreSQLConnector.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Grasshopper.Kernel;
+using SandboxGh.Attributes;
+using SandboxCore.Data;
+
+namespace SandboxGh.Data
+{
+    public class PostgreSQLConnector : SandboxComponent
+    {
+        public PostgreSQLConnector()
+          : base("This component connects to a Postgres database and returns the connection string'", "SQL")
+        {
+        }
+
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddTextParameter("Server", "S", "The server to connect to.", GH_ParamAccess.item);
+            pManager.AddTextParameter("Port", "P", "The port to connect to. The default port is 5432 for Azure PostgreSQL", GH_ParamAccess.item, "5432");
+            pManager.AddTextParameter("Username", "U", "Username to connect with.", GH_ParamAccess.item);
+            pManager.AddTextParameter("Password", "PW", "Password to connect with.", GH_ParamAccess.item);
+            pManager.AddTextParameter("Database", "D", "Target database to connect to.", GH_ParamAccess.item);
+
+        }
+
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddTextParameter("Connection Success", "CS", "Notifies a successful connection.", GH_ParamAccess.item);
+        }
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            string server = String.Empty;
+            string port = "5423";
+            string uid = String.Empty;
+            string pw = String.Empty;
+            string db = String.Empty;
+            if (!DA.GetData(0, ref server) | !DA.GetData(1, ref port) | !DA.GetData(2, ref uid) | !DA.GetData(3, ref pw) | !DA.GetData(4, ref db)) return;
+        
+        
+            ///SQLConnector.SQLConnect(server, port, uid, pw, db);
+
+            DA.SetData(0, "Test output");
+        }
+
+        protected override System.Drawing.Bitmap Icon => Resources.ValueTypeDictIcon;
+
+        public override Guid ComponentGuid => new Guid("2DD78996-31C9-44A8-A884-BB4AAC9741B7");
+    }
+}
