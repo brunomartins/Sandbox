877e3223c756ccae464860e7afea64e7eb42911b
William Hamilton <william.hamilton@mottmac.com>
2022-03-02 18:03:05 +1100

    Started setting up for query work

---
+++ SandboxGh/Data/PostgreSQLQuery.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Grasshopper.Kernel;
+using SandboxGh.Attributes;
+using SandboxCore.Data;
+
+namespace SandboxGh.Data
+{
+    public class PostgreSQLQuery : SandboxComponent
+    {
+        public PostgreSQLQuery()
+          : base("This component executes a query on a database.'", "SQL")
+        {
+        }
+
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddTextParameter("Connection String", "CS", "The Connection String from the PostgreSQLConnector component.", GH_ParamAccess.item);
+            pManager.AddTextParameter("Query", "Q", "The Query to execute.", GH_ParamAccess.item);
+        }
+
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddTextParameter("Result", "R", "The query result.", GH_ParamAccess.item);
+        }
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            string server = String.Empty;
+            string port = String.Empty;
+            string uid = String.Empty;
+            string pw = String.Empty;
+            string db = String.Empty;
+            if (!DA.GetData(0, ref server) | !DA.GetData(1, ref port) | !DA.GetData(2, ref uid) | !DA.GetData(3, ref pw) | !DA.GetData(4, ref db)) return;
+            var serverResults = SQLConnector.PostgreSQL(server, port, uid, pw, db);
+            string state = serverResults.Item1;
+            string cs = serverResults.Item2;
+
+            DA.SetData(0, state);
+        }
+
+        protected override System.Drawing.Bitmap Icon => Resources.PostgreSQLQueryIcon;
+
+        public override Guid ComponentGuid => new Guid("2DD78996-31C9-44A8-A884-BB4AAC9741B4");
+    }
+}
