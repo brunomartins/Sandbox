8dd09da7b120dbc387f3558196ca65e45f1d491c
William Hamilton <william.hamilton@mottmac.com>
2022-03-02 15:08:36 +1100

    Documentaion of methods

--- SandboxCore/Data/SQLConnector.cs
+++ SandboxCore/Data/SQLConnector.cs
@@ -5,11 +5,23 @@ using System.Linq;
 
 namespace SandboxCore.Data
 {
+    /// <summary>
+    /// Collections of methods used to operate on a database.
+    /// </summary>
     public static class SQLConnector
     {
-        public static Tuple<string, string> PostgresConnect(string server, string port, string uid, string pw, string db)
+        /// <summary>
+        /// Checks that a connection can be made to the database, and returns the connection string to be used for queries and commands.
+        /// </summary>
+        /// <param name="server">Server to connect to.</param>
+        /// <param name="port">Port to connect to.</param>
+        /// <param name="username">Username of account.</param>
+        /// <param name="password">Password of account.</param>
+        /// <param name="database">Name of database to connect to.</param>
+        /// <returns>Returns a tuple in the format of (ConnectionStatus, ConnectionString).</returns>
+        public static Tuple<string, string> PostgresConnect(string server, string port, string username, string password, string database)
         {
-            var cs = $"Host={server};Port={port};Username={uid};Password={pw};Database={db}";
+            var cs = $"Host={server};Port={port};Username={username};Password={password};Database={database}";
 
             var con = new NpgsqlConnection(cs);
             con.Open();
